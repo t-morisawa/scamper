96bd818470c4ed90d82bb9720e775e9ddff24fe7
Toma Morisawa <morisawa@mizuuchi.lab.tuat.ac.jp>
2017-08-12 21:55:29 +0900

    update: semantic ui

--- src/presentationals/Theme.js
+++ src/presentationals/Theme.js
@@ -1,9 +1,11 @@
 import React, { PropTypes } from 'react'
+import { Header } from 'semantic-ui-react'
 
 const Theme = ({ theme }) => (
         <div>
-        {theme}
-    </div>
+        <Header as='h1'>{theme}</Header>
+        </div>
 )
 
 export default Theme
+
