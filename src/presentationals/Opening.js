cd94ba8450b6f6c9bb05320def5ce792f7e3b696
Toma Morisawa <morisawa@mizuuchi.lab.tuat.ac.jp>
2018-05-22 21:17:45 +0900

    add index.html

--- src/presentationals/Opening.js
+++ src/presentationals/Opening.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { Container, Header } from 'semantic-ui-react'
 import Start from '../containers/Start'
 import Explanation from './Explanation'
+import { title } from '../constants'
 
 class Opening extends Component {
     render() {
         return (
                 <Container style={{ marginTop: '2em' }}>
-                <Header as='h1'>SCAMPER</Header>
+                <Header as='h1'>{title}</Header>
                 <Explanation />
                 <Start />
                 </Container>
