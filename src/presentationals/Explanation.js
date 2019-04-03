da2dfdb1802939986cba0c4003ed479a47e3729d
Toma Morisawa <13.35.104@gmail.com>
2019-04-03 23:10:01 +0900

    add triz and swot description

--- src/presentationals/Explanation.js
+++ src/presentationals/Explanation.js
@@ -19,7 +19,6 @@ const Explanation = ({ theme }) => {
         );
 
     case 'scamperJa':
-    default:
         return (
                 <List>
                 <p>
@@ -32,6 +31,36 @@ const Explanation = ({ theme }) => {
             </p>
                 </List>
         );
+
+    case 'TRIZ':
+        return (
+                <List>
+                <p>
+                TRIZは課題解決の手法の一種です。<br />
+                課題を40アプローチから見て考えることによって、
+                課題を解決するためのアイデアを生成するのに役立ちます。
+            </p>
+                <p>
+                既存のアイデアを一つ思い浮かべて、それに対する改善案をフォームに入力していきましょう。
+            </p>
+                </List>
+        );
+
+    case 'SWOT':
+        return (
+                <List>
+                <p>
+                SWOTは意思決定のための手法です。<br />
+                内部・外部要因のプラス・マイナス要因を分析し、目標が達成可能かを判断します。
+            </p>
+                <p>
+目標を一つ思い浮かべて、それに対する現状分析をフォームに入力していきましょう。
+            </p>
+                </List>
+        );
+
+    default:
+     return (<div></div>);
     }
 }
 
