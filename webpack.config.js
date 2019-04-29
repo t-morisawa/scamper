4a29727a2929bd542bafa40bb570aa5c6f0bb08f
Toma Morisawa <13.35.104@gmail.com>
2019-04-29 20:32:40 +0900

    add js-yaml-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-    entry: __dirname + '/src/index.js',
-    output: {
-        path: __dirname + '/public',
-        filename: 'scamper.js',
-        publicPath: ''
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: 'url-loader?limit=100000'
-            }
-        ]
-    }
+  entry: __dirname + '/src/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'scamper.js',
+    publicPath: ''
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      { test: /\.(ya?ml)$/, loader: "js-yaml-loader" }
+    ]
+  }
 }
