aaa15be8eb14dd673736ec84cef54e9d9b53e3f5
Toma Morisawa <13.35.104@gmail.com>
2020-04-29 15:02:11 +0900

    fix indents

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,49 +1,49 @@
 /**
-  * index: ページ番号
-  * ideas: 保存されたテキスト
-  */
+ * index: ページ番号
+ * ideas: 保存されたテキスト
+ */
 import { combineReducers } from 'redux'
 
 const home = (state = { isStart : false }, action) => {
-    switch (action.type) {
-    case 'START':
-        return true;
-    default:
-        return state;
-    }
+  switch (action.type) {
+  case 'START':
+    return true;
+  default:
+    return state;
+  }
 }
 
 const initialData = {
-    index: 0,
-    ideas: [],
+  index: 0,
+  ideas: [],
 }
 
 const main = (state = initialData, action) => {
-    switch (action.type) {
-    case 'SUBMIT':
-        let log = state.ideas
-        log.push({
-            index: state.index,
-            ideas: action.text
-        })
-        return {
-            index: state.index + 1,
-            ideas: log
-        }
-    default:
-        return state
+  switch (action.type) {
+  case 'SUBMIT':
+    let log = state.ideas
+    log.push({
+      index: state.index,
+      ideas: action.text
+    })
+    return {
+      index: state.index + 1,
+      ideas: log
     }
+  default:
+    return state
+  }
 }
 
 const result = (state = { index: 0 }, action ) => {
-    switch (action.type) {
-    case 'TOGGLE_RESULT':
-        return {
-            index: state.index + 1,
-        }
-    default:
-        return state
+  switch (action.type) {
+  case 'TOGGLE_RESULT':
+    return {
+      index: state.index + 1,
     }
+  default:
+    return state
+  }
 }
 
 const scamperApp = combineReducers({
