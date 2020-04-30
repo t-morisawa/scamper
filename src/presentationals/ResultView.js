596ba3c89b9ee4ac3fe35a080a579315e439e505
Toma Morisawa <13.35.104@gmail.com>
2020-04-30 21:48:06 +0900

    update to convert container to presentational

--- src/presentationals/ResultView.js
+++ src/presentationals/ResultView.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import ShowResult from '../containers/ShowResult.js'
+import ResultIdeas from '../presentationals/ResultIdeas.js'
 import ToggleResult from '../containers/ToggleResult.js'
 import { Container } from 'semantic-ui-react'
 
-let ResultView = () => {
-    return (
-            <Container style={{ marginTop: '2em' }}>
-            <ToggleResult />
-            <ShowResult />
-            </Container>
-    )
+let ResultView = ({ ideas, index }) => {
+  return (
+      <Container style={{ marginTop: '2em' }}>
+      <ToggleResult />
+      <ResultIdeas ideas={ideas} index={index} />
+      </Container>
+  )
 }
 
 export default ResultView
