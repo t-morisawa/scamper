ec0995789fe3be6b36eb2813013008358c7b4fa9
Toma Morisawa <13.35.104@gmail.com>
2020-05-04 09:32:50 +0900

    fix indent

--- src/containers/ReduxForm.js
+++ src/containers/ReduxForm.js
@@ -5,31 +5,31 @@ import { submit } from '../actions'
 import { Form } from 'semantic-ui-react'
 
 class ReduxForm extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    componentDidMount() {
-        this.nameInput.focus();
-    }
+  componentDidMount() {
+    this.nameInput.focus();
+  }
 
-    render() {
-        return (
-                <Form onSubmit={events => {
-                    return new Promise(resolve => {
-                        events.preventDefault()
-                        this.props.dispatch(submit(this.nameInput.value))
-                        this.nameInput.value = ''
-                    })
-                }}>
-                <div>
-                <input ref={node => {
-                    this.nameInput = node
-                }} />
-                </div>
-                </Form>
-        );
-    }
+  render() {
+    return (
+        <Form onSubmit={events => {
+          return new Promise(resolve => {
+            events.preventDefault()
+            this.props.dispatch(submit(this.nameInput.value))
+            this.nameInput.value = ''
+          })
+        }}>
+        <div>
+        <input ref={node => {
+          this.nameInput = node
+        }} />
+        </div>
+        </Form>
+    );
+  }
 }
 
 ReduxForm = connect()(ReduxForm)
