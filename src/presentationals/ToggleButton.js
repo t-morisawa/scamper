860d8983d67d300273cf5c23a21368f2ebbc1450
Toma Morisawa <13.35.104@gmail.com>
2020-05-06 10:41:17 +0900

    fix to use ref

--- src/presentationals/ToggleButton.js
+++ src/presentationals/ToggleButton.js
@@ -4,15 +4,16 @@ import { Button } from 'semantic-ui-react'
 class ToggleButton extends Component {
   constructor(props) {
     super(props)
+    this.button = React.createRef();
   }
 
   componentDidMount() {
-    this.refs.togglebutton.focus();
+    this.button.current.focus();
   }
 
   render () {
     return (
-        <Button onClick={this.props.onClick} ref='togglebutton'>
+        <Button onClick={this.props.onClick} ref={this.button}>
         switch result view
       </Button>
     );
