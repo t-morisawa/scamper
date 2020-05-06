0397630a76dc70966def996fd687ee288cb929e9
Toma Morisawa <13.35.104@gmail.com>
2020-05-06 10:38:08 +0900

    fix indents

--- src/presentationals/StartButton.js
+++ src/presentationals/StartButton.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react'
 
 class StartButton extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    componentDidMount() {
-        this.refs.startbutton.focus();
-    }
+  componentDidMount() {
+    this.refs.startbutton.focus();
+  }
 
-    render () {
-        return (
-            <Button onClick={this.props.onClick} ref='startbutton'>
-                Press Return Key
-            </Button>
-        );
-    }
+  render () {
+    return (
+        <Button onClick={this.props.onClick} ref='startbutton'>
+        Press Return Key
+      </Button>
+    );
+  }
 }
 
 export default StartButton
--- src/presentationals/ToggleButton.js
+++ src/presentationals/ToggleButton.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react'
 
 class ToggleButton extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    componentDidMount() {
-        this.refs.togglebutton.focus();
-    }
+  componentDidMount() {
+    this.refs.togglebutton.focus();
+  }
 
-    render () {
-        return (
-                <Button onClick={this.props.onClick} ref='togglebutton'>
-                switch result view
-            </Button>
-        );
-    }
+  render () {
+    return (
+        <Button onClick={this.props.onClick} ref='togglebutton'>
+        switch result view
+      </Button>
+    );
+  }
 }
 
 export default ToggleButton;
